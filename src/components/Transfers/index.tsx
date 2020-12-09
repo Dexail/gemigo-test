22fc12faaa70159868d0338f156fd68704202a2d
Alex <alex.kucherenkowb@gmail.com>
2020-12-09 15:32:17 +0300

    create transfers component

--- src/components/Transfers/index.tsx
+++ src/components/Transfers/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import TransferField from "./TransferField";
 
 import "./Transfers.scss";
+import planeSvg from "../../assets/img/plane.svg";
 
 type Props = {
     list: Array<object>
@@ -11,7 +12,7 @@ const Transfers = ({list}: Props) => {
     return (
         <div className="transfers">
             <div className="transfers__content">
-                <div className="transfers__content-list">
+                <div className="transfers__list">
                     <h2>Number of transfers</h2>
                     {list.length > 0 ?
                         (list.map((element, index) => {
@@ -23,11 +24,13 @@ const Transfers = ({list}: Props) => {
                         :
                         (<p>Empty List</p>)}
                 </div>
-                <div className="transfers__content-count">
+                <div className="transfers__count">
                     <p>1 transfer</p>
-                    <div className="transfers__icon">
+                    <div className="transfers__count-line">
+                        <span/>
                         <img
-                            className="transfers__icon-plane"
+                            className="icon-plane"
+                            src={planeSvg}
                             alt="No plane icon"
                         />
                     </div>
